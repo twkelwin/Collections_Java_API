0b4c6d7a4af8ee32a526499dcf920774b1549d18
twkelwin <twkelwin@gmail.com>
2025-07-21 18:03:11 -0300

    CarinhoDeCompras finalizado! com seus metodos funcionais!

--- src/List/CarinhoDeCompras.java
+++ src/List/CarinhoDeCompras.java
@@ -3,6 +3,8 @@ package List;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class CarinhoDeCompras {
 
 	// 2. Carrinho de Compras:
@@ -18,24 +20,54 @@ public class CarinhoDeCompras {
 	// consideração o preço e a quantidade de cada item.
 	// exibirItens(): Exibe todos os itens presentes no carrinho, mostrando seus
 	// nomes, preços e quantidades.
-	
+
 	private List<Item> itemList;
-	
-	
-	
+
 	public CarinhoDeCompras() {
 		this.itemList = new ArrayList<>();
 	}
 
-
-
 	public void adicionarItem(String nome, double preco, int quantidade) {
-		
+		itemList.add(new Item(nome, preco, quantidade));
 	}
-	
-	
-	
+
+	public void removerItem(String nome) {
+		List<Item> itensParaRemover = new ArrayList<>();
+		for (Item item : itemList) {
+			if (item.getNome().equalsIgnoreCase(nome)) {
+				itensParaRemover.add(item);
+			}
+		}
+		itemList.removeAll(itensParaRemover);
+	}
+
+	public int calcularValorTotal() {
+		int total = 0;
+		for (Item item : itemList) {
+			total += item.getPreco() * item.getQuantidade();
+			System.out.println("O total é de: " + total);
+		}
+		return total;
+	}
+
+	public void exibirItens() {
+		System.out.println(itemList);
+	}
+
 	public static void main(String[] args) {
+
+		CarinhoDeCompras listaItem = new CarinhoDeCompras();
+		listaItem.adicionarItem("tv", 2300, 2);
+		System.out.println("");
+		listaItem.adicionarItem("celular", 1200, 1);
+		listaItem.exibirItens();
+		listaItem.calcularValorTotal();
+		System.out.println("===========================");
+		listaItem.removerItem("celular");
+		listaItem.exibirItens();
+		listaItem.calcularValorTotal();
 		
+		
+
 	}
 }
